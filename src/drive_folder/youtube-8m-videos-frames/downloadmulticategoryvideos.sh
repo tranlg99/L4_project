5c66d5ec2faa8fdc62740f87cece0248606dce83
tranlg99 <tranlg.99@gmail.com>
2022-11-07 14:51:33 +0000

    source code needed to download dataset from youtube 8m and convert to frames

---
+++ src/drive_folder/youtube-8m-videos-frames/downloadmulticategoryvideos.sh
@@ -0,0 +1,12 @@
+# Check number of arguments
+if [ "$#" != "2" ]; then
+	echo "Usage: bash downloadmulticategoryvideos.sh <number-of-videos-per-category> <selected-category-file-name>"
+	exit 1
+fi
+
+while read line
+	echo Hello
+	do
+		bash downloadcategoryids.sh $1 "${line}"
+		bash downloadvideos.sh $1 "${line}"
+	done < "$2"
